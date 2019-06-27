b2e4909c990f0d17c426dc837c29aab640b8d380
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-27 19:25:46 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,7 +47,7 @@ class SongsController < ApplicationController
   end
 
   def upload
-    CSV.foreach("songs", headers: true) do |song|
+    CSV.foreach("spec/fixtures/songs.csv", headers: true) do |song|
       artist = Artist.find_or_create_by(name: song[1])
       Song.create(title: song[0], artist: artist)
     end
